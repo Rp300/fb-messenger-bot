9b221a9bd12ebc158c1527aa3dcd9d885de802cb
akhiljakatdar <akhilrj2@illinois.edu>
2019-02-23 23:47:21 -0600

    teset

--- app.py
+++ app.py
@@ -81,6 +81,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
                     print("before formatting")
+                    send_message(sender_id, message_text)
                     check = format_input(message_text)
                     #if (check == None):
                     #    print(message_text + " is an illegal input")
@@ -90,7 +91,6 @@ def webhook():
                     #     send_message(sender_id, "NONE")
                     #     check = Check()
                     # print("1here")
-                    send_message(sender_id, message_text)
                     send_message(sender_id, check.to_string())
 
                 if messaging_event.get("delivery"):  # delivery confirmation
