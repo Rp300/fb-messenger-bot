46cf2c482741b274cdc89e2005be84e27cfb2022
sanky1234 <sankuracecar@gmail.com>
2019-02-24 01:15:14 -0600

    Editing Format

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
                     #name,email,amount
-
+                    params = format_input(message_text)
                     if len(params) != 3 or "@" not in params[1] or "$" not in params[2]:
                         send_message(sender_id, "Please send message in the following format2:")
                         send_message(sender_id, "name, email, $amount")
